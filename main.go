f7f055ca4b086a6c83ab529228196ba6e1b13439
latermonk <iback.china@gmail.com>
2021-06-08 14:40:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -96,6 +96,17 @@ func main() {
 		})
 		//get
 		v1Group.GET("/todo", func(c *gin.Context) {
+			//get todo list data
+			var todoList []Todo
+			if err = DB.Find(&todoList).Error;err!= nil{
+				c.JSON(http.StatusOK, gin.H{
+					"error" : err.Error(),
+				})
+			}else{
+				c.JSON(http.StatusOK, todoList)
+			}
+
+
 
 		})
 		v1Group.GET("/todo/:id", func(c *gin.Context) {
@@ -103,11 +114,40 @@ func main() {
 		})
 		//PUT
 		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error":"invalid id"})
+				return
+			}
+			var todo Todo
+			if err = DB.Where("id=?", id).First(&todo).Error;err!=nil{
+				c.JSON(http.StatusOK, gin.H{"error":err.Error()})
+				return
+			}
+			c.BindJSON(&todo)
+			//UPDATE DB
+			if err = DB.Save(&todo).Error;err!=nil{
+				c.JSON(http.StatusOK, gin.H{"error":err.Error()})
+			}else{
+				c.JSON(http.StatusOK, todo)
+			}
+
+
+
 
 		})
 		//DEL
 		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
-
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error":"invalid id"})
+				return
+			}
+			if err = DB.Where("id=?", id).Delete(Todo{}).Error;err!=nil{
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			}else{
+				c.JSON(http.StatusOK, gin.H{id: "deleted"})
+			}
 		})
 
 	}
