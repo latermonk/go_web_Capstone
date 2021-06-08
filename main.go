2e7e34c6d8c0e1722f8e5269e00833f4ed51dc91
latermonk <iback.china@gmail.com>
2021-06-08 10:33:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	//Create database
+	//docker run --name mysql8019 --restart=always -p 13306:3306 -e MYSQL_ROOT_PASSWORD=root1234 -d mysql:8.0.19
+	//sql: CREATE DATABASE bubble;
+
+	//Connect  to database
+
+
 	r := gin.Default()
 
 
