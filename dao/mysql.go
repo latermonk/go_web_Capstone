0459590116ed96c80837b73535c4cb4016cac1ff
latermonk <iback.china@gmail.com>
2021-06-09 13:34:48 +0800

    fix

--- dao/mysql.go
+++ dao/mysql.go
@@ -14,8 +14,14 @@ var (
 
 
 func InitMySQL(cfg *setting.MySQLConfig) (err error) {
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	//	cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
+	fmt.Printf("%s,%s,%s,%d,%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
+
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
+
 	DB, err = gorm.Open("mysql", dsn)
 	if err != nil {
 		return
